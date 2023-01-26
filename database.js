1b98e2a5641a032fd154e39b0ea7883fe4f17cf2
rvikr77 <94057204+rvikr77@users.noreply.github.com>
2023-01-26 12:40:55 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,67 @@
+shops=[
+    {id:0,data:{
+      latitudes:13.007002761475542, 
+      longitudes: 80.2533483685362,
+      distance:0,
+      "name":"Centre 1",
+      "Battery":"20(Tata Nexon), 10(City)\n5(other general cars)",
+      "Motor":"Bajaj Motor(2-100cc,5-750cc), Tata motors 7-350cc ",
+      "Tyres":"10 small, 5medium, 7 large, MRF tyres,\n5 medium Apollo Tyres"
+   }},
+   {id:1, data:{
+     latitudes:12.936973545461063,
+     longitudes: 77.59047995677919,
+     distance:0,
+     "name":"Centre 2",
+     "Battery":"20(Tata Nexon), 10(Tata Tigor)\n5(other general cars)",
+     "Motor":"Bajaj Motor(2-100cc,5-750cc), Tata motors 7-350cc ",
+     "Tyres":"10 small, 5medium, 7 large, MRF tyres,\n5 medium Apollo Tyres",
+     "Chassis":"5 gear shifts(nano), 2 body parts(tesla model 3)"
+   }},
+    {id:2,data:{
+     latitudes:13.009669526650567, 
+      longitudes: 80.25200628707896,
+      distance:0,
+      "name":"Centre 3",
+      "Battery":"20(MG hector), 10(Tata Tigor)\n5(other general cars)",
+      "Motor":"Bajaj Motor(2-100cc,5-750cc), Tata motors 7-350cc ",
+      "Tyres":"10 small, 5medium, 7 large, MRF tyres,\n5 medium Apollo Tyres",
+      "Chassis":"5 gear shifts(MG hector), 2 body parts(tesla model 3)"
+   }},
+    {id:3,data:{
+     latitudes:13.007250364262896,
+      longitudes:80.25133336274796,
+      distance:0,
+      "name":"Centre 4",
+      "Battery":"20(Nexa), 10(Venue)\n2(other general cars)",
+      "Motor":"hyundai Motor(2-100cc,5-750cc), Tata motors 7-350cc ",
+      "Tyres":"10 small, 5medium, 7 large, Hyundai tyres,\n5 medium Apollo Tyres",
+      "Chassis":"5 gear shifts(hyundai pro), 2 body parts(tesla model 3)"
+   }},
+   {id:4,data:{
+    latitudes:13.0072503642484699,
+     longitudes:80.35133336274796,
+     distance:0,
+     "name":"Centre 5",
+     "Battery":"20(Tata Nexon), 10(Tata Tigor)\n5(other general cars)",
+     "Motor":"Bajaj Motor(2-100cc,5-750cc), Tata motors 7-350cc ",
+     "Tyres":"5 medium Apollo Tyres",
+     "Chassis":"5 gear shifts(MG hector), 2 body parts(tesla model 3)"
+  }},
+  {id:5,data:{
+    latitudes:13.0072506423225090,
+     longitudes:80.25233336274796,
+     distance:0,
+     "name":"Centre 6",
+     "Battery":"20(Tata Nexon), 10(Tata Tigor)\n5(other general cars)",
+     "Tyres":"10 small, 5medium, 7 large, MRF tyres,\n5 medium Apollo Tyres",
+     "Chassis":"5 gear shifts(MG hector), 2 body parts(tesla model 3)"
+  }},
+    {id:6,data:{
+     latitudes:13.00592928087087129,
+      longitudes: 80.25171693792989,
+      distance:0,
+      "name":"Centre 7",
+      "Tyres":"10 small, 5medium, 7 large, MRF tyres,\n5 medium Apollo Tyres",
+      "Chassis":"5 gear shifts(Mahindra EV), 2 body parts(tesla model x)"
+   }}];

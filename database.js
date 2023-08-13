73572472440b6abec70b85d6f2d6f80987e813a7
rvikr77 <94057204+rvikr77@users.noreply.github.com>
2023-08-13 15:56:44 +0530

    Update database.js

--- database.js
+++ database.js
@@ -5,64 +5,64 @@ shops=[
       longitudes: 80.2533483685362,
       distance:0,
       "name":"Centre 1",
-      "Battery" :"<img src ='./images/battery.jpeg' style='height: 10%; width: 10%;'/> 20(Car 1 Model 1), 10(Car 2 Model 3)\n5(other general cars)",
-      "Motor" :"<img src ='./images/motor.jpg'style='height: 10%; width: 10%;'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
-      "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 10%; width: 10%;'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example2 Tyres"
+      "Battery" :"<img src ='./images/battery.jpeg' style='height: 5%; width: 5%;'/> 20(Car 1 Model 1), 10(Car 2 Model 3)\n5(other general cars)",
+      "Motor" :"<img src ='./images/motor.jpg'style='height: 5%; width: 5%;'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
+      "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example2 Tyres"
    }},
    {id:1, data:{
      latitudes:12.936973545461063,
      longitudes: 77.59047995677919,
      distance:0,
      "name":"Centre 2",
-     "Battery" :"<img src ='./images/battery.jpeg'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
-     "Motor" :"<img src ='./images/motor.jpg'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
-     "Tyres" :"<img src ='./images/tyres.jpeg'/> 10 small, 5medium, 7 large, Example3 tyres,\n5 medium Example1 Tyres",
-     "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(car 1 model 5), 2 body parts(car 5 model 2)"
+     "Battery" :"<img src ='./images/battery.jpeg'style='height: 5%; width: 5%;'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
+     "Motor" :"<img src ='./images/motor.jpg'style='height: 5%; width: 5%;'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
+     "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example3 tyres,\n5 medium Example1 Tyres",
+     "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(car 1 model 5), 2 body parts(car 5 model 2)"
    }},
     {id:2,data:{
      latitudes:13.009669526650567, 
       longitudes: 80.25200628707896,
       distance:0,
       "name":"Centre 3",
-      "Battery" :"<img src ='./images/battery.jpeg'/> 20(Car 2 model 1), 10(Car 1 Model 2)\n5(other general cars)",
-      "Motor" :"<img src ='./images/motor.jpg'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
-      "Tyres" :"<img src ='./images/tyres.jpeg'/> 10 small, 5medium, 7 large, Example3 tyres,\n5 medium Example2 Tyres",
-      "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(Car 2 model 1), 2 body parts(Car 5 model 1)"
+      "Battery" :"<img src ='./images/battery.jpeg'style='height: 5%; width: 5%;'/> 20(Car 2 model 1), 10(Car 1 Model 2)\n5(other general cars)",
+      "Motor" :"<img src ='./images/motor.jpg'style='height: 5%; width: 5%;'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
+      "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example3 tyres,\n5 medium Example2 Tyres",
+      "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(Car 2 model 1), 2 body parts(Car 5 model 1)"
    }},
     {id:3,data:{
      latitudes:13.007250364262896,
       longitudes:80.25133336274796,
       distance:0,
       "name":"Centre 4",
-      "Battery" :"<img src ='./images/battery.jpeg'/> 20(Car 1 Model 2), 10(Car 1 Model 3)\n2(other general cars)",
-      "Motor" :"<img src ='./images/motor.jpg'/> Car3 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
-      "Tyres" :"<img src ='./images/tyres.jpeg'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example5 Tyres",
-      "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(Car 2 model 1), 2 body parts(Car 5 model 1)"
+      "Battery" :"<img src ='./images/battery.jpeg'style='height: 5%; width: 5%;'/> 20(Car 1 Model 2), 10(Car 1 Model 3)\n2(other general cars)",
+      "Motor" :"<img src ='./images/motor.jpg'style='height: 5%; width: 5%;'/> Car3 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
+      "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example5 Tyres",
+      "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(Car 2 model 1), 2 body parts(Car 5 model 1)"
    }},
    {id:4,data:{
     latitudes:13.0072503642484699,
      longitudes:80.35133336274796,
      distance:0,
      "name":"Centre 5",
-     "Battery" :"<img src ='./images/battery.jpeg'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
-     "Motor" :"<img src ='./images/motor.jpg'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
-     "Tyres" :"<img src ='./images/tyres.jpeg'/> 5 medium Example4 Tyres",
-     "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(Car 2 Model 4), 2 body parts(Car 3 model 3)"
+     "Battery" :"<img src ='./images/battery.jpeg'style='height: 5%; width: 5%;'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
+     "Motor" :"<img src ='./images/motor.jpg'style='height: 5%; width: 5%;'/> Example 1 Motor(2-100cc,5-750cc), Car 1 motors 7-350cc ",
+     "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 5 medium Example4 Tyres",
+     "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(Car 2 Model 4), 2 body parts(Car 3 model 3)"
   }},
   {id:5,data:{
     latitudes:13.0072506423225090,
      longitudes:80.25233336274796,
      distance:0,
      "name":"Centre 6",
-     "Battery" :"<img src ='./images/battery.jpeg'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
-     "Tyres" :"<img src ='./images/tyres.jpeg'/> 10 small, 5medium, 7 large, Example2 tyres,\n5 medium Example1 Tyres",
-     "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(Car 2 Model 1), 2 body parts(Car 4 Model 5)"
+     "Battery" :"<img src ='./images/battery.jpeg'style='height: 5%; width: 5%;'/> 20(Car 1 Model 1), 10(Car 1 Model 2)\n5(other general cars)",
+     "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example2 tyres,\n5 medium Example1 Tyres",
+     "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(Car 2 Model 1), 2 body parts(Car 4 Model 5)"
   }},
     {id:6,data:{
      latitudes:13.00592928087087129,
       longitudes: 80.25171693792989,
       distance:0,
       "name":"Centre 7",
-      "Tyres" :"<img src ='./images/tyres.jpeg'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example2 Tyres",
-      "Chassis" :"<img src ='./images/chassis.jpeg'/> 5 gear shifts(Car 3 EV), 2 body parts(Car 5 EV)"
+      "Tyres" :"<img src ='./images/tyres.jpeg'style='height: 5%; width: 5%;'/> 10 small, 5medium, 7 large, Example1 tyres,\n5 medium Example2 Tyres",
+      "Chassis" :"<img src ='./images/chassis.jpeg'style='height: 5%; width: 5%;'/> 5 gear shifts(Car 3 EV), 2 body parts(Car 5 EV)"
    }}];
